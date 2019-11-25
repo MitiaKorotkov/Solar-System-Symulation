8d58725b62532655cd9cec6a72a5e9e883d7b57a
Alekhina <55592195+Alekhina@users.noreply.github.com>
2019-11-25 22:50:21 +0300

    он дописан
    его не было, теперь он есть

--- ss_calculation.py
+++ ss_calculation.py
@@ -1,37 +1,58 @@
-"""Вычисления сил и перемещенийи передвижения объектов"""
+"""Работа с файлом с данными"""
 
-# Написано Коротковым Дмитрием
-G = 6.67E-11  # Гравитационная постоянная
+# Основано на solar_input.py Хирьянова
 
 
-def set_force_value(obj, other_obj):
+from ss_objects import Star, Planet
+
+
+def init_space_objects(data_filename):
     """
-    Вычисляет и устанавливает значение силы,
-    действующей на тело со стороны всех остальных тел
-    :param obj: Тело
-    :param other_obj: Тела, участвующие во взаимодействии
+    Считывает данные об объектах симуляции из данного
+    файла и создаёт для них объекты, массив с которыми возвращается.
+    Может возникнуть ошибка NameError при наличии в файле объекта
+    с именем отличным от 'planet' или 'star'
+    :param data_filename: Имя файла с данными
+    :return: Массив объектов, описанных в файле
     """
-    pass
+    objects = []  # Массив объектов симуляции
+    # Открытие файла
+    with open(data_filename) as f:
+        # Считывание данных и приведение их к желаемому виду
+        for line in f:
+            if len(line.strip()) == 0 or line[0] == '#':
+                continue
+            obj = line.split()[0].lower()
+            # Создание требуемых объектов с требуемыми параметрами
+            if obj == 'star':
+                star = Star(float(line.split()[3]), float(line.split()[4]), float(line.split()[5]),
+                            float(line.split()[6]),
+                            float(line.split()[7]), int(line.split()[1]), line.split()[2])
+                objects.append(star)
+            elif obj == 'planet':
+                planet = Planet(float(line.split()[3]), float(line.split()[4]), float(line.split()[5]),
+                                float(line.split()[6]),
+                                float(line.split()[7]), float(line.split()[1]), line.split()[2])
+                objects.append(planet)
+            else:
+                raise NameError('Unknown object')
+    return objects
 
-def move_object(obj, dt):
-    """
-    Пересчёт и установка значений скорости и
-    координат в соответствии с заданной силой
-    :param obj: Тело
-    :param dt: Промежуток времени
-    """
-    pass
 
-
-def set_objects_position(objects, dt):
+def save_data_to_file(data_filename, objects):
     """
-    Перемещает объекты на холсте в соответствии
-    с заданными координатами
-    :param objects: Тела
-    :param dt: Промежутой Времени
+    Записывает в файл данные о телах, участвующих в симуляции
+    :param data_filename: Имя файла для записи
+    :param objects: Массив тел, данные о которых нужно записать
     """
-    pass
-
+    with open(data_filename, 'w') as f:
+        # Запись данных в хорошем виде в файл
+        for obj in objects:
+            if obj.id == 'star':
+                f.write('Star '+ str(obj.x) + ' ' + str(obj.y) + ' ' + str(obj.m) + ' ' + str(obj.vx) + ' ' + str(obj.vy) + '\n')
+            if obj.id == 'planet':
+                f.write('Planet '+ str(obj.x) + ' ' + str(obj.y) + ' ' + str(obj.m) + ' ' + str(obj.vx) + ' ' + str(obj.vy) + '\n')    
+            
 
 if __name__ == '__main__':
     print('This module is not to run the program')
