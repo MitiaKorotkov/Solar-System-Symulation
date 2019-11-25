cf7a1875d72ee77faa8588100a72fcf46376bfb1
Alekhina <55592195+Alekhina@users.noreply.github.com>
2019-11-25 22:52:49 +0300

    он дописан
    его не было, теперь есть и правда он

--- ss_calculation.py
+++ ss_calculation.py
@@ -1,58 +1,53 @@
-"""Работа с файлом с данными"""
+# Написано Алехиной Дарьей feat. Дмитрий Коротков
 
-# Основано на solar_input.py Хирьянова
-
-
-from ss_objects import Star, Planet
+G = 6.67E-11  # Гравитационная постоянная
 
 
-def init_space_objects(data_filename):
+def set_force_value(obj, objects):
     """
-    Считывает данные об объектах симуляции из данного
-    файла и создаёт для них объекты, массив с которыми возвращается.
-    Может возникнуть ошибка NameError при наличии в файле объекта
-    с именем отличным от 'planet' или 'star'
-    :param data_filename: Имя файла с данными
-    :return: Массив объектов, описанных в файле
+    Вычисляет и устанавливает значение силы,
+    действующей на тело со стороны всех остальных тел
+    :param obj: Тело
+    :param other_obj: Тела, участвующие во взаимодействии
     """
-    objects = []  # Массив объектов симуляции
-    # Открытие файла
-    with open(data_filename) as f:
-        # Считывание данных и приведение их к желаемому виду
-        for line in f:
-            if len(line.strip()) == 0 or line[0] == '#':
-                continue
-            obj = line.split()[0].lower()
-            # Создание требуемых объектов с требуемыми параметрами
-            if obj == 'star':
-                star = Star(float(line.split()[3]), float(line.split()[4]), float(line.split()[5]),
-                            float(line.split()[6]),
-                            float(line.split()[7]), int(line.split()[1]), line.split()[2])
-                objects.append(star)
-            elif obj == 'planet':
-                planet = Planet(float(line.split()[3]), float(line.split()[4]), float(line.split()[5]),
-                                float(line.split()[6]),
-                                float(line.split()[7]), float(line.split()[1]), line.split()[2])
-                objects.append(planet)
-            else:
-                raise NameError('Unknown object')
-    return objects
+    obj.fx = 0
+    obj.fy = 0
+    for j in range (len(objects)):
+        if obj != objects[j]:
+            obj.fx -= (obj.x-objects[j].x)*\
+                     G*obj.m*objects[j].m/(((obj.x-objects[j].x)**2+(obj.y-objects[j].y)**2)**(3/2))
+    for j in range (len(objects)):
+        if obj != objects[j]:
+            obj.fy -= (obj.y-objects[j].y)*\
+                     G*obj.m*objects[j].m/(((obj.x-objects[j].x)**2+(obj.y-objects[j].y)**2)**(3/2))
 
 
-def save_data_to_file(data_filename, objects):
+def move_object(obj, dt):
     """
-    Записывает в файл данные о телах, участвующих в симуляции
-    :param data_filename: Имя файла для записи
-    :param objects: Массив тел, данные о которых нужно записать
+    Пересчёт и установка значений скорости и
+    координат в соответствии с заданной силой
+    :param obj: Тело
+    :param dt: Промежуток времени
     """
-    with open(data_filename, 'w') as f:
-        # Запись данных в хорошем виде в файл
-        for obj in objects:
-            if obj.id == 'star':
-                f.write('Star '+ str(obj.x) + ' ' + str(obj.y) + ' ' + str(obj.m) + ' ' + str(obj.vx) + ' ' + str(obj.vy) + '\n')
-            if obj.id == 'planet':
-                f.write('Planet '+ str(obj.x) + ' ' + str(obj.y) + ' ' + str(obj.m) + ' ' + str(obj.vx) + ' ' + str(obj.vy) + '\n')    
-            
+    obj.vx+=dt*(obj.fx/obj.m)
+    obj.vy+=dt*(obj.fy/obj.m)
+    obj.x+=dt*obj.vx
+    obj.y+=dt*obj.vy
+    
+
+
+def set_objects_position(objects, dt):
+    """
+    Перемещает объекты на холсте в соответствии
+    с заданными координатами
+    :param objects: Тела
+    :param dt: Промежутой Времени
+    
+    """
+    for i in range(len(objects)):
+        set_force_value(objects[i], objects)
+    for i in range(len(objects)):
+       move_object(objects[i],dt)
 
 if __name__ == '__main__':
     print('This module is not to run the program')
